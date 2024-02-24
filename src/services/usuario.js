bca27517e1d518b890608c292dab78e55a6d7eb7
Jhon-boy <jhoncuvi12@gmail.com>
2024-02-24 16:28:13 -0500

    Feat: Cambios en la consola registrado

--- src/services/usuario.js
+++ src/services/usuario.js
@@ -1,73 +1,74 @@
-
-
 export const autentificacion = (req, res) => {
-    const {cedula} = req.body;
+  const { cedula } = req.body;
+  console.log(
+    "===== Se ha iniciado un prorceos de autenfificacion para el usuario: ",
+    cedula
+  );
 
-    if(cedula != "0606253169"){
-        const response = {
-            codeError: 500, // Bad Request
-            message: "Todos los campos son obligatorios.",
-            content: null,
-        };
-
-        return res.status(500).json(response);
-
-    } 
+  if (cedula != "0606253169") {
     const response = {
-        codeError: 200,
-        message: "Autenficacion Existosa",
-        content: {
-            autenticacionExitosa: true,
-            codigoAuth: 100
-        }
+      codeError: 500, // Bad Request
+      message: "Todos los campos son obligatorios.",
+      content: null,
     };
-    console.log("Se ha logeado de manera exitosa");
+    console.log(
+      " ===== El usuario es distintos a los registros de la base de datos"
+    );
+    return res.status(500).json(response);
+  }
+  const response = {
+    codeError: 200,
+    message: "Autenficacion Existosa",
+    content: {
+      autenticacionExitosa: true,
+      codigoAuth: 100,
+    },
+  };
+  console.log(" ===== Se ha logeado de manera exitosa");
 
-    return res.status(200).json(response);
-
-}
+  return res.status(200).json(response);
+};
 
 export const seguirConversacion = (req, res) => {
-    const {codigoAuth} = req.params;
+  const { codigoAuth } = req.params;
 
-    console.log("codigo auth:", codigoAuth);
-
-    if(codigoAuth != 100){
-        const response = {
-            codeError: 500, // Bad Request
-            message: "Error",
-            content: null,
-        };
-
-        return res.status(500).json(response);
-    } 
-
+  console.log(" ===== codigo auth:", codigoAuth);
+  if (codigoAuth != 100) {
     const response = {
-        codeError: 200,
-        message: "Puede serguir en la conversacion",
-        content: {
-            autenticacionExitosa: true
-        }
+      codeError: 500, // Bad Request
+      message: "Error",
+      content: null,
     };
 
+    console.log(" ===== Codigo Incorrecto");
+    return res.status(500).json(response);
+  }
 
-    return res.status(200).json(response);
+  const response = {
+    codeError: 200,
+    message: "Puede serguir en la conversacion",
+    content: {
+      autenticacionExitosa: true,
+    },
+  };
 
-}
+  console.log(" ===== El usuario se ha autentificado");
+  return res.status(200).json(response);
+};
 
 export const consultaSaldo = (req, res) => {
-    const {usuario} = req.params;
+  const { usuario } = req.params;
 
-    const response = {
-        codeError: 200,
-        message: "Informacion exitosa del usuario",
-        content: {
-            autenticacionExitosa: true,
-            saldoUsuario : "780.454"
-        }
-    };
+  console.log(" ===== El usuario desea conocer su saldo: ", usuario);
+  const response = {
+    codeError: 200,
+    message: "Informacion exitosa del usuario",
+    content: {
+      autenticacionExitosa: true,
+      saldoUsuario: "780.454",
+    },
+  };
 
-
-    return res.status(200).json(response);
-
-}
+  console.log(" ===== Informacion devuelva exitosamente");
+  return res.status(200).json(response);
+};
